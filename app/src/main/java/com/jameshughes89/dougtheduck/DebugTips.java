d05e5712ec243d1f741e60617799678f74499164
James Hughes <JamesHughes89@Gmail.com>
2017-04-27 17:32:00 -0400

    Added images, and basically all functionality. Just need to add swipe for tips.

---
+++ app/src/main/java/com/jameshughes89/dougtheduck/DebugTips.java
@@ -0,0 +1,25 @@
+package com.jameshughes89.dougtheduck;
+
+/**
+ * Created by JamesHughes89 on 4/27/2017.
+ *
+ * This class just holds some Strings that will be printed as
+ * Debugging tips that Doug can suggest to the user.
+ */
+
+class DebugTips {
+
+    /**
+     * Just a static array of doug's tips (of type String)
+     */
+    static String[] Tips = {
+            "Please tell me what your code is supposed to do. Be sure to take your time.",
+            "One line at a time. I'm not joking, do it... Honestly, make ZERO assumptions about your code",
+            "Perhaps you comment a little more. You may be suprised!",
+            "Are you sure that loop is supposed to go to .length-2? That seems unusual.",
+            "It might be a good call to break this code into more methods. Smaller chunks of code are easier to work with and test.",
+            "This might sound odd, but maybe take a little bit of a break. You might be suprised with what you think of in 10mins from now",
+            "Do you think better variable names might help in this situation?",
+    };
+
+}
--- app/src/main/java/com/jameshughes89/dougtheduck/MainActivity.java
+++ app/src/main/java/com/jameshughes89/dougtheduck/MainActivity.java
@@ -1,13 +1,55 @@
 package com.jameshughes89.dougtheduck;
 
+import android.os.Debug;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    Random rng;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Hide both the navigation bar and the status bar.
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
+
+
+        rng = new Random();
+
+
+
     }
+
+
+    /**
+     * When clicking the image, Doug will throw out a new tip.
+     *
+     * @param view This will be the onClick for the ImageView
+     */
+    public void dougTip(View view){
+
+        // Picks a random tip for Doug to say
+        int nextTipIndex = rng.nextInt(DebugTips.Tips.length);
+        String theTip = DebugTips.Tips[nextTipIndex];
+
+        // Puts the tip on the screen (in the textView specifically)
+        TextView displayTip = (TextView) findViewById(R.id.textView);
+        displayTip.setText(theTip);
+    }
+
+
 }
